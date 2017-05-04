7f00b2f7c90859eca4f5122c313f893cd2ddaa76
Eric Fennis <eric.fennis@gmail.com>
2017-05-04 12:47:17 +0200

    Date bug fixed, fontsize set correct, styling listview

--- app/assets/styles/COLOR.js
+++ app/assets/styles/COLOR.js
@@ -14,6 +14,7 @@ module.exports = {
    PINK:       '#f06aa7',
    DARKPURPLE: '#170630',
    DARKGRAY:   '#4c5358',
+   BLACK40:    'rgba(0, 0, 0, 0.40)',
    BLACK78:    'rgba(0, 0, 0, 0.78)',
    GREY:       '#b2b2b2',
    PALEGRAY:   '#f6f9f9',
