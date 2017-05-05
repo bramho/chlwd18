e1bba61d030be8ea8a9905f9ccc24b318a2c5c28
bramho <bramhoningh16@gmail.com>
2017-05-05 09:45:18 +0200

    Added some styling to the tabbar + changed events header

--- app/screens/EventsList.js
+++ app/screens/EventsList.js
@@ -281,11 +281,11 @@ export default class EventsList extends Component {
       return (
          <View style={General.container}>
             <View style={ComponentStyle.headerContainer}>
-               <TextInput
-                  style={ComponentStyle.searchBarInput}
-                  placeholder={getTranslation('searchTerm')}
-                  onChange={this.setSearchText.bind(this)}
-               />
+               <View style={ComponentStyle.headerTitleContainer}>
+                  <Text style={General.h4}>
+                     {getTranslation('eventsMenuItem')}
+                  </Text>
+               </View>
                <View style={ComponentStyle.filterIconContainer}>
                   <View style={ComponentStyle.filterIcon}>
                      <Text>F</Text>
