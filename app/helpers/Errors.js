4ff8652594792dcba0f363a7df03c60c076d8769
bramho <bramhoningh16@gmail.com>
2017-06-21 18:43:38 +0200

    Added error message to newslist

--- app/helpers/Errors.js
+++ app/helpers/Errors.js
@@ -5,6 +5,7 @@
 errors = {
    1: 'Geen resultaten',
    100: 'Er is iets misgegaan bij het ophalen van de evenementen.',
+   200: 'Er is iets misgegaan bij het ophalen van het nieuws',
    300: 'Er is iets misgegaan bij het ophalen van jou favorite evenementen',
 }
 
