d94d617ea9210b3f5b5eeeda3be4f7113f8e00c0
bramho <bramhoningh16@gmail.com>
2017-07-03 14:25:57 +0200

    Fixed bug with icons on iOS

--- android/app/src/main/java/com/chlwd18/MainApplication.java
+++ android/app/src/main/java/com/chlwd18/MainApplication.java
@@ -3,6 +3,9 @@ package com.chlwd18;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.airbnb.android.react.maps.MapsPackage;
+import com.i18n.reactnativei18n.ReactNativeI18n;
 import com.airbnb.android.react.maps.MapsPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.i18n.reactnativei18n.ReactNativeI18n;
@@ -26,6 +29,9 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
+            new MapsPackage(),
+            new ReactNativeI18n(),
             new MapsPackage(),
             new VectorIconsPackage(),
             new ReactNativeI18n()
